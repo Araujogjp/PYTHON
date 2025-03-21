7338b4701a12491840d7413544be4cd57607d7ed
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-03-21 19:33:22 -0300

    Update QUESTÃO 1.py

--- ESTRUTURA DE REPETIÇÕES/1ª PARTE/QUESTÃO 1.py
+++ ESTRUTURA DE REPETIÇÕES/1ª PARTE/QUESTÃO 1.py
@@ -1,1 +1,6 @@
-
+print('OS NÚMEROS MÚLTIPLOS DE 3 NO INTERVALO DE [3, 100] SÃO:')
+q = 0
+for contador in range(3, 100 + 1):
+    if (contador % 3 == 0):
+        q += 1
+        print(f'{q}º número: {contador}')
