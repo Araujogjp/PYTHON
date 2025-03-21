4977bfde076b781576f9eb3ea88104dc3fd05531
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-03-21 19:39:42 -0300

    Update QUESTÃO 11.py

--- ESTRUTURA DE REPETIÇÕES/2ª Parte/QUESTÃO 11.py
+++ ESTRUTURA DE REPETIÇÕES/2ª Parte/QUESTÃO 11.py
@@ -1,1 +1,9 @@
-
+contador = 0 # CONTAR
+q = 0 # ENUMERAR
+try:
+    n = int(input('Digite qualquer número inteiro: '))
+    for contador in range(n, n + 51):
+        q += 1
+        print(f'{q}ª número: {contador}')
+except Exception as ERRO_EXCECAO:
+    print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')
