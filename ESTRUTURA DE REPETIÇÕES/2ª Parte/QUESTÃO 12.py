83caa93dad967213f0896fa5ea3addd52146e4d9
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-03-21 19:43:24 -0300

    Update QUESTÃO 12.py

--- ESTRUTURA DE REPETIÇÕES/2ª Parte/QUESTÃO 12.py
+++ ESTRUTURA DE REPETIÇÕES/2ª Parte/QUESTÃO 12.py
@@ -1,1 +1,18 @@
-
+try:
+    # DECLARAR VARIÁVEIS
+    q = 0 # ENUMERAR
+    c = 0.0 # TEMPERATURA
+    media = 0.0 # MEDIA DA TEMPERATURA
+    soma = 0.0 # SOMAR AS TEMPERATURAS
+    while True:
+        q += 1
+        c = float(input(f'TEMPERATURA DO {q}ª DIA: '))
+        soma += c
+        media = soma / q
+        # AS TEMPERATURAS FICAM ACIMA DE 28º graus
+        if (c <= 28):
+            print('FIM DO PROGRAMA')
+            print(f'A Média de temperatura foi de: {media:.2f}')
+            break
+except Exception as ERRO_EXCECAO:
+    print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')
