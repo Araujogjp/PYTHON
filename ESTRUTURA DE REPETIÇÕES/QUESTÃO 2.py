fc31be56fcd294ba91700be70320002dbfb4ca0b
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-03-21 19:11:10 -0300

    Create QUESTÃO 2.py

---
+++ ESTRUTURA DE REPETIÇÕES/QUESTÃO 2.py
@@ -0,0 +1,12 @@
+print('OS NÚMEROS MÚLTIPLOS DE 11 NO INTERVALO DE [200, 100], SUA SOMA E SUA MÉDIA É DE:')
+q = 0  # ENUMERAR
+soma = 0
+media = 0
+for contador in range(200, 100, -1):  # CONTADOR
+    if (contador % 11 == 0):
+        q += 1
+        soma += contador
+        media = soma / q
+        print(f'{q}º número: {contador}')
+print(f'A soma é de: {soma}')
+print(f'A média é de: {media}')
