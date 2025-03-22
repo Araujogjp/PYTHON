1c54934c654090e2b29e44b94bc99562a9a56bc4
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-03-22 16:22:49 -0300

    Update QUESTÃO 3.py

--- ESTRUTURA DE REPETIÇÕES/1ª PARTE/QUESTÃO 3.py
+++ ESTRUTURA DE REPETIÇÕES/1ª PARTE/QUESTÃO 3.py
@@ -1,18 +1,21 @@
-print('NÚMEROS MÚLTIPLOS DE N QUE ESTEJAM ENTRE O INTERVALO [limite_inferior e limite_superior]')
-n = int(input('Digite o valor de n [n >= 2 e inteiro]: '))
-if n < 2:
-  print('O valor de N deve ser igual ou maior que 2')
-else:
-  limite_inf = int(input('Digite o limite_inferior: '))
-  if limite_inf < 2:
-    print('O limite_inferior deve ser maior ou igual a 2')
+try:
+  print('NÚMEROS MÚLTIPLOS DE N QUE ESTEJAM ENTRE O INTERVALO [limite_inferior e limite_superior]')
+  n = int(input('Digite o valor de n [n >= 2 e inteiro]: '))
+  if n < 2:
+    print('O valor de N deve ser igual ou maior que 2')
   else:
-    limite_superior = int(input('Digite o limite_superior: '))
-    if limite_superior < 2:
-      print('O limite_superior deve ser maior ou igual a 2')
+    limite_inf = int(input('Digite o limite_inferior: '))
+    if limite_inf < 2:
+      print('O limite_inferior deve ser maior ou igual a 2')
     else:
-      if limite_inf > n:
-        if limite_inf > limite_inf:  
-          for contador in range(limite_inf, limite_superior):
-            if contador % n == 0:
-              print(f'{contador} número: {contador}')
+      limite_superior = int(input('Digite o limite_superior: '))
+      if limite_superior < 2:
+        print('O limite_superior deve ser maior ou igual a 2')
+      else:
+        if limite_inf > n:
+          if limite_inf > limite_inf:  
+            for contador in range(limite_inf, limite_superior):
+              if contador % n == 0:
+                print(f'{contador} número: {contador}')
+except Exception as ERRO_EXCECAO:
+  print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')
