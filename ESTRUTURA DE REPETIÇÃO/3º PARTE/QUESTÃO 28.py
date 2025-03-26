f538c7b447b97fdf34ddc7a36610584bd474788a
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-03-26 18:34:28 -0300

    Update QUESTÃO 28.py

--- ESTRUTURA DE REPETIÇÃO/3º PARTE/QUESTÃO 28.py
+++ ESTRUTURA DE REPETIÇÃO/3º PARTE/QUESTÃO 28.py
@@ -1,12 +1,18 @@
+import math
 # VARIAVÉIS
-pi = 3.14
 n = 0
-soma = .0
-multiplicacao = 0.0
+s = math.pi
+m = 1.0
 try:
     n = int(input('DIGITE QUALQUER NÚMERO INTEIRO E POSITIVO E >= 1: '))
     if(n < 1):
         print('O NÚMERO TEM QUE SER POSITIVO E MAIOR OU IGUAL A 1')
-    else:   
+    else: 
+        for contador in range(2, n + 1 , 2):
+            s += math.pi / contador
+        for contador in range(1, n + 1, 2):
+            m *= contador / math.pi
 except Exception as ERRO_EXCECAO:
     print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')
+print(f'A SOMA DE TODOS OS NÚMEROS FOI DE: {s: .2f}')
+print(f'A MULTIPLICAÇÃO DE TODOS OS NÚMEROS FOI DE: {m: .2f}')
