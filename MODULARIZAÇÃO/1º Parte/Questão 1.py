3240300b49a836e556708012f6c5a361219dd830
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-03-27 18:33:54 -0300

    Update Questão 1.py

--- MODULARIZAÇÃO/1º Parte/Questão 1.py
+++ MODULARIZAÇÃO/1º Parte/Questão 1.py
@@ -1,1 +1,23 @@
+# CRIAR A FUNÇÃO AQUI:
+import math 
+def arranjo(n, p): 
+  if(n < 0 or p < 0 or n < p):
+    print('VALORES INVALIDOS')
+    return None
+  else:
+    a = math.factorial(n) / (math.factorial(n - p))
+    return a
 
+def combinacao(n, p):
+  if(n < 0 or p < 0 or n < p):
+    print('VALORES INVALIDOS')
+    return None
+  else: 
+    c = arranjo(n,p) / math.factorial(p)
+    return c 
+
+""" 
+REUSIBILIDADE
+print(arranjo(5,2))
+print(combinacao(5,2))
+"""
