0580f288ee745d06a0871e7d7748bc50ca4b979b
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-03-28 20:11:13 -0300

    Update QUESTÃO 1.py

--- MODULARIZAÇÃO/1º Parte/QUESTÃO 1.py
+++ MODULARIZAÇÃO/1º Parte/QUESTÃO 1.py
@@ -1,26 +1,22 @@
+# CRIAR A FUNÇÃO AQUI:
 # QUESTÃO 3 DA LISTA
 # CRIAR A FUNÇÃO AQUI:
 import math 
 def arranjo(n, p): 
-  if(n < 0 or p < 0 or n < p):
-    print('VALORES INVALIDOS')
-    return None
-  else:
-    a = math.factorial(n) / (math.factorial(n - p))
-    return a
+  return  math.factorial(n) / (math.factorial(n - p))
 
 def combinacao(n, p):
-  if(n < 0 or p < 0 or n < p):
-    print('VALORES INVALIDOS')
-    return None
-  else: 
-    c = arranjo(n,p) / math.factorial(p)
-    return c 
+  return arranjo(n,p) / math.factorial(p)
 
 
 # MENU: REUSABILIDADE
 try: 
-  print(arranjo(5,2))
-  print(combinacao(5,2))
+   n = int(input('DIGITE QUALQUER NÚMERO: '))
+   p = int(input('DIGITE QUALQUER NÚMERO: '))
+   if(n < 0 or p < 0 or n < p):
+    print('VALORES INVALIDOS')
+   else:
+    print(f'O VALOR DO ARRANJO FOI DE: {arranjo(n,p)}')
+    print(f'O VALOR DA COMBINÇÃO FOI DE: {combinacao(n,p)}')
 except Exception as ERRO_EXCECAO:
   print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO} ')
