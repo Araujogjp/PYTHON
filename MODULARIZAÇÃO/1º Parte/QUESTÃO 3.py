c418e5f4d97277eb28a1c0747e07c6e7c066169a
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-04-01 18:50:17 -0300

    Update QUESTÃO 3.py

--- MODULARIZAÇÃO/1º Parte/QUESTÃO 3.py
+++ MODULARIZAÇÃO/1º Parte/QUESTÃO 3.py
@@ -10,14 +10,16 @@ def volume(r, h):
 
 
 # MENU: REUSABILIDADE
-
-r = float(input('Digite o valor de r: '))
-if(r < 0):
-  print('O valor de r deve ser igual ou maior que 0')
-else:
-  print(f'A área do círculo é de: {area(r):.3f}')
-  h = float(input('Digite o valor de h: '))
-  if(h < 0):
-    print('O valor de h deve ser maior ou igual a 0')
+try:
+  r = float(input('Digite o valor de r: '))
+  if(r < 0):
+    print('O valor de r deve ser igual ou maior que 0')
   else:
-    print(f'O volume do cilindro é de: {volume(r, h):.3f}')
+    print(f'A área do círculo é de: {area(r):.3f}')
+    h = float(input('Digite o valor de h: '))
+    if(h < 0):
+      print('O valor de h deve ser maior ou igual a 0')
+    else:
+      print(f'O volume do cilindro é de: {volume(r, h):.3f}')
+except Exception as ERRO_EXCECAO:
+  print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO} ')
