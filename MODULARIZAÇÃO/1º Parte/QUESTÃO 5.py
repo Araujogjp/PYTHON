8c7db66d3d37c16d16175b0093a4aaffcb4e1aba
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-04-03 16:22:15 -0300

    Update QUESTÃO 5.py

--- MODULARIZAÇÃO/1º Parte/QUESTÃO 5.py
+++ MODULARIZAÇÃO/1º Parte/QUESTÃO 5.py
@@ -18,26 +18,29 @@ print('Condição obrigatória: É triângulo se, somente se (A < B + C) e (B < 
 print('Triângulo Escaleno = 0 lados iguais')
 print('Triângulo Isósceles = 2 lados iguais')
 print('Triângulo Equilátero = 3 lados iguais')
-a = int(input('Digite o valor de a: '))
-if(a < 0):
-  print('O lado A deve ser maior que 0')
-else: 
-  b = int(input('Digite o valor de b: '))
-  if(b < 0):
-    print('O lado B deve ser maior que 0')
-  else:
-    c = int(input('Digite o valor de c: '))
-    if(c < 0):
-      print('O lado C deve ser maior que 0')
+try: 
+  a = int(input('Digite o valor de a: '))
+  if(a < 0):
+    print('O lado A deve ser maior que 0')
+  else: 
+    b = int(input('Digite o valor de b: '))
+    if(b < 0):
+      print('O lado B deve ser maior que 0')
     else:
-      verificacao = triangulo(a, b, c)
-      if(verificacao == 0):
-        print('Não é um triângulo')
-      else: 
-        print('É um Triângulo')
-        if(verificacao == 3):
-          print('É um Triângulo Equilátero')
-        elif(verificacao == 2):
-          print('É um Triângulo Isósceles')
-        else:
-          print('É um Triângulo Escaleno')
+      c = int(input('Digite o valor de c: '))
+      if(c < 0):
+        print('O lado C deve ser maior que 0')
+      else:
+        verificacao = triangulo(a, b, c)
+        if(verificacao == 0):
+          print('Não é um triângulo')
+        else: 
+          print('É um Triângulo')
+          if(verificacao == 3):
+            print('É um Triângulo Equilátero')
+          elif(verificacao == 2):
+            print('É um Triângulo Isósceles')
+          else:
+            print('É um Triângulo Escaleno')
+except Exception as ERRO_EXCECAO:
+  print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO} ')
