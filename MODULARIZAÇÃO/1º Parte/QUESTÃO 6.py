cc87cf5136c8dc7e00dbce9a3e0affadda2710e6
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-04-03 18:20:30 -0300

    Update QUESTÃO 6.py

--- MODULARIZAÇÃO/1º Parte/QUESTÃO 6.py
+++ MODULARIZAÇÃO/1º Parte/QUESTÃO 6.py
@@ -15,3 +15,15 @@ def soma(n):
 def media(n):
   return soma(n) / 2
 
+try
+  n = float(input('Digite o valor de n: '))
+  funcao_a = f_a(n)
+  funcao_b = f_b(n)
+  soma_termos = soma(n)
+  media_termos = media(n)
+  print(f'O VALOR DA FUNÇÃO A É: {funcao_a:.2f}')
+  print(f'O VALOR DA FUNÇÃO B É: {funcao_b:.2f}')
+  print(f'A SOMA É {soma_termos: .2f}')
+  print(f'A MÉDIA É: {media_termos: .2f}')
+except Exception as ERRO_EXCECAO: 
+  print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO} ')
