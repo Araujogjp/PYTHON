59e4c91beb0aef3f9fdde4b25a8b09f0f5bc27ad
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-04-04 10:40:15 -0300

    Create QUESTÃO 11.py

---
+++ MODULARIZAÇÃO/2º Parte/QUESTÃO 11.py
@@ -0,0 +1,24 @@
+# CRIAR A FUNÇÃO AQUI:
+# QUESTÃO 8 DA LISTA
+def fibonacci(n):
+  ultimo = 0 
+  penultimo = 1
+  soma = 0
+  if(n >= 0):
+    soma += ultimo
+  if(n >= 1):
+    soma += penultimo
+  for i in range(2, n + 1):
+    proximo = ultimo + penultimo
+    soma += proximo
+    ultimo = penultimo 
+    penultimo = proximo
+  return soma 
+
+# MENU: REUSABILIDADE
+n = int(input('DIGITE UM NÚMERO: '))
+if(n < 0):
+  print('N TEM QUE SER MAIOR OU IGUAL A 0')
+else:
+  somatorio = fibonacci(n)
+  print(f'{somatorio}')
