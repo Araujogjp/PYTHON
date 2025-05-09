e366e29cb0126a7de547a55bbe64ce8bbec18e0e
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-05-09 18:20:51 -0300

    Update QUESTÃO 1.py

--- LISTAS/QUESTÃO 1.py
+++ LISTAS/QUESTÃO 1.py
@@ -1,6 +1,10 @@
 func = []
-empresa = []
+empresa = [] 
 
+
+# MODULARIZAR DADOS DE LEITURA: ?
+
+# LER NOME
 def lernome():
   while True:
     try:
@@ -18,8 +22,8 @@ def lernome():
 def lercargo():
   while True:
     try:
-      cargo = input('CARGO: ')
-      if(len(cargo <= 3)):
+      cargo = (input('CARGO: '))
+      if(len(cargo) <= 3):
         print('ERRO: ESCOLHA DENOVO')
       else:
         break
@@ -31,54 +35,55 @@ def lercargo():
 # SORTEAR MATRÍCULA
 import numpy
 def matricula():
-  mat = [valor for valor in (numpy.random.normal(10000, 1000, 1))]
-  return mat 
+  return int(numpy.random.normal(10000, 1000))
 
+# TEM PLANO OU NÃO
 def planosaude():
   while True:
     try:
       opcao = int(input('[1] CASO POSSUA PLANO DE SAÚDE / [0] CASO NÃO TENHA: '))
-      if(opcao > 1 or opcao < 0):
+      if(opcao == 1):
+        sim += 1
+        return 'Tem plano de Saúde'
+      elif(opcao == 0):
+        nao += 1
+        return 'Não tem plano de Saúde'
+      else: 
         print('[1] CASO TIVER PLANO / [0] CASO NÃO TENHA')
-        break
-      else:
-        if(opcao == 1):
-          plano = 'sim'
-        else:
-          plano = 'nao'
     except Exception as ERRO_EXCECAO:
       print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')
-      break
-  return plano
 
+def porcetagemplano(sim, nao):
+  total = sim + nao
+  porcentagem = (sim / total ) * 100
+  return porcentagem
 
+# LER SALÁRIO
 def lersalario():
   while True:
     try:
       salario = float(input('SALÁRIO: '))
-      if(salario <= 1000):
+      if(salario < 1000):
         print('ERRO: ESCOLHA DENOVO')
       else:
-        break
+        return salario
     except Exception as ERRO_EXCECAO:
       print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')
-      break
-    return salario
+
 
 
 # MENU: PROGRAMA PRINCIPAL (MAIN)
 q = 0
-while(q < 3):
-  q += 1
+while(q < 1):
   try:
-     print(f'ENTRE COM OS DADOS DO {len(empresa) + 1}º ALUNO: ')
-      nome = lernome()
-      cargo = lercargo()
-      mat = matricula()
-      plano = planosaude()
-      salario = lersalario()
-      func = 
+     print(f'ENTRE COM OS DADOS DO {len(empresa) + 1}º FUNCIONÁRIO: ')
+     nome = lernome()
+     cargo = lercargo()
+     mat = matricula()
+     plano = planosaude()
+     salario = lersalario()
+     func = [nome, cargo, mat, plano, salario]
+     empresa.append(func)
+     q += 1
   except Exception as ERRO_EXCECAO:
     print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')
-    break
-  
