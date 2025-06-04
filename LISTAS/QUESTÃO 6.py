74a3266d1ffa17f8951466f2cf8afe0075ec3915
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-06-04 14:58:09 -0300

    Update QUESTÃO 6.py

--- LISTAS/QUESTÃO 6.py
+++ LISTAS/QUESTÃO 6.py
@@ -2,11 +2,12 @@
 pilotos = []
 kart = []
 
+
 # MODULARIZAR DADOS DE LEITURA: ?
 def lerNome():
   while True:
     try:
-      nome = inpput('NOME: ')
+      nome = input('NOME: ')
       if len(nome) <= 2:
         print('ERRO => NOME DEVE POSSUIR MAIS DE 2 CARACTÉRES')
       else:
@@ -31,7 +32,7 @@ def lerEquipe():
 def tempo():
   while True:
     try:
-      primeira = float(inpuu('TEMPO(SEGUNDOS) PRIMEIRA VOLTA: '))
+      primeira = float(input('TEMPO(SEGUNDOS) PRIMEIRA VOLTA: '))
       if primeira <= 0:
         print('ERRO, TENTE NOVAMENTE')
       else:
@@ -49,6 +50,7 @@ def tempo():
   return primeira, segunda, terceira
 
 
+
 # MENU: PROGRAMA PRINCIPAL (MAIN)
 while True:
   print('MENU')
@@ -60,12 +62,51 @@ while True:
   try:
     opcao = int(input('OPÇÃO: '))
     if opcao == 0:
+      print(f'DADOS DO {len(kart) + 1}º COMPETIDOR')
       nome = lerNome()
       equipe = lerEquipe()
       voltas = tempo()
       pilotos = [nome, equipe, voltas]
       kart.append(pilotos)
+      break
     elif opcao == 1:
-      
+      try:
+        # MENOR TEMPO PRIMEIRA
+        tempo_min_primeira = [valor[2][0] for valor in kart]
+        minPrimeira = min(tempo_min_primeira)
+        indiceP = tempo_min_primeira.index(minPrimeira)
+        # MENOR TEMPO SEGUNDA
+        tempo_min_segunda = [valor[2][1] for valor in kart]
+        minSegunda = min(tempo_min_segunda)
+        indiceS = tempo_min_segunda.index(minSegunda)
+        # MENOR TEMPO TERCEIRA
+        tempo_min_terceira = [valor[2][2] for valor in kart]
+        minTerceira = min(tempo_min_terceira)
+        indiceT = tempo_min_terceira.index(minTerceira)
+        print(f'NOME: {kart[indiceP][0]} COM {kart[indiceP][2][0]} SEGUNDOS')
+        print(f'NOME: {kart[indiceS][0]} COM {kart[indiceS][2][1]} SEGUNDOS')
+        print(f'NOME: {kart[indiceT][0]} COM {kart[indiceT][2][2]} SEGUNDOS')
+      except:
+        print('NENHUM COMPETIDOR CADATRADO')
+    elif opcao == 2:
+      try:
+        maisRapido = [sum(valor[2]) for valor in kart]
+        TheBest = min(maisRapido)
+        indiceM = maisRapido.index(TheBest)
+        print(f'CAMPEÃO: {kart[indiceM][0]}')
+        print(f'EQUIPE: {kart[indiceM][1]}')
+        print(f'TEMPO: {TheBest} SEGUNDOS')
+      except:
+        print('NENHUM COMPETIDOR CADATRADO')
+    elif opcao == 3:
+      listaMedia = [sum(valor[2]) for valor in kart]
+      media = sum(listaMedia) / len(kart)
+      print(f'A MÉDIA DE PONTUAÇÃO DA COMPETIÇÃO FOI DE: {media}')
+      acimaMedia = [valor for valor in kart if sum(valor[2]) < media]
+      for piloto in acimaMedia:
+        print(f'NOME: {piloto[0]}')
+        print(f'EQUIPE: {piloto[1]}')
+    else:
+      break
   except:
     print('ERRO')
